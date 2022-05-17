c8e4ab69bbf88230896db5e89ca0063af6bcb2d4
aloisdg <aloisdegouvello@live.fr>
2022-05-17 23:18:43 +0200

    handl pattern

--- src/index.js
+++ src/index.js
@@ -1,20 +1,35 @@
-const fs = require('fs');
-const path = require('path');
-const core = require('@actions/core');
-const io = require('@actions/io');
+import fs from "fs";
+const fsPromises = fs.promises;
+
+const core = require("@actions/core");
+const io = require("@actions/io");
+const glob = require("glob");
+
+async function orderFile(path, encoding, property) {
+  const content = await fsPromises.readFile(path, encoding);
+  const items = JSON.parse(content);
+  items.sort((a, b) => a[property].localeCompare(b[property]));
+  const data = JSON.stringify(items, undefined, 2);
+  fsPromises.writeFile(path, data, encoding);
+}
+
+async function orderFiles(files, encoding, property) {
+  var orderTasks = files.map((x) => orderFile(x, encoding, property));
+  await Promise.all(orderTasks);
+}
 
 try {
-  const inputPath = core.getInput('input-path');
-  const outputPath = core.getInput('output-path') || inputPath;
-  const property = core.getInput('property');
-  const encoding = core.getInput('encoding');
+  const pattern = core.getInput("pattern");
+  const property = core.getInput("property");
+  const encoding = core.getInput("encoding");
 
-  const items = JSON.parse(fs.readFileSync(inputPath, encoding));
-
-  items.sort((a, b) => a[property].localeCompare(b[property]));
-  
-  const outputJSON = JSON.stringify(items, undefined, 2); 
-  fs.writeFileSync(outputPath, outputJSON, encoding);
+  glob(pattern, (err, files) => {
+    if (err) {
+      throw err;
+    } else {
+      orderFiles(files, encoding, property);
+    }
+  });
 } catch (error) {
   core.setFailed(error.message);
 }
