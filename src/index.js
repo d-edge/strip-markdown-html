c0553932af141cf2512953678525ca4aaf092b33
Aloïs de Gouvello <aloisdegouvello@live.fr>
2022-05-17 18:08:13 +0200

    Order by property

--- src/index.js
+++ src/index.js
@@ -2,24 +2,19 @@ const fs = require('fs');
 const path = require('path');
 const core = require('@actions/core');
 const io = require('@actions/io');
-const remark = require('remark');
-const strip = require('remark-strip-html');
 
 try {
   const inputPath = core.getInput('input-path');
   const outputPath = core.getInput('output-path') || inputPath;
+  const property = core.getInput('property');
   const encoding = core.getInput('encoding');
 
-  const inputText = fs.readFileSync(inputPath, encoding);
+  const items = JSON.parse(fs.readFileSync(inputPath, encoding));
 
-  remark()
-    .use(strip)
-    .process(inputText, (err, outputText) =>
-      {
-        if (err) throw err;
-        io.mkdirP(path.dirname(outputPath));
-        fs.writeFileSync(outputPath, outputText, encoding);
-      });
+  items.sort((a, b) => a[property].localeCompare(b[property]));
+  
+  const outputJSON = JSON.stringify(items, undefined, 2); 
+  fs.writeFileSync(outputPath, outputJSON, encoding);
 } catch (error) {
   core.setFailed(error.message);
 }
